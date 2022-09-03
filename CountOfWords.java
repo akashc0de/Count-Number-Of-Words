ffd79dbc3e17df7f61ff8037ed7a019b34f044f1
Akash <111621704+akashc0de@users.noreply.github.com>
2022-09-03 20:09:18 +0530

    Add files via upload

---
+++ CountOfWords.java
@@ -0,0 +1,40 @@
+package mapInterface;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+public class CountOfWords {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String s="subalai sambar thala thala thalapathy";
+		
+		String[] ss= s.split(" ");
+		
+		TreeMap<String,Integer> tm=new TreeMap();
+		
+		for(String s1:ss) {
+			if(tm.containsKey(s1)){
+				tm.put(s1, tm.get(s1)+1);
+			}
+			else {
+				tm.put(s1, 1);
+			}
+		}
+		System.out.println(tm);
+		
+		Set<Map.Entry<String,Integer>> ss1=tm.entrySet();
+		
+		for(Map.Entry<String,Integer> s4:ss1) {
+			if(s4.getValue()>1)
+			System.out.println(s4.getKey() +" "+s4.getValue());
+		}
+		
+
+	}
+
+}
